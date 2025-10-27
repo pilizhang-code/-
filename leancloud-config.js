4cc72e34e692ab723c67513ed814e945e8740a00
pilizhang-code <z3301019524@gmail.com>
2025-10-27 16:18:31 +0800

    Add files via upload

---
+++ leancloud-config.js
@@ -0,0 +1,11 @@
+/**
+ * LeanCloud配置文件
+ * 请在此处填写您的LeanCloud应用配置
+ */
+
+// LeanCloud配置
+const LC_CONFIG = {
+    appId: 'i3HOlSULshlcZHb2eLPPeAeq-gzGzoHsz',
+    appKey: 'RIRWzdOmEoM02P4gbAEXQp69',
+    serverURLs: 'https://i3holsul.lc-cn-n1-shared.com' // 国内节点需要配置
+};
